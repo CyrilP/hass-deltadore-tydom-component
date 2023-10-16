d7a127281fc064f3a243c9561232544eacd0af5c
cyrilp <5814027+CyrilP@users.noreply.github.com>
2023-10-16 19:56:22 +0200

    fix gateway, rename device

--- custom_components/deltadore-tydom/ha_entities.py
+++ custom_components/deltadore-tydom/ha_entities.py
@@ -71,7 +71,10 @@ class GenericSensor(SensorEntity):
         "mainReference",
         "mainVersionHW",
         "productName",
-        "mac"
+        "mac",
+        "jobsMP",
+        "softPlan",
+        "softVersion",
     ]
 
     def __init__(
@@ -182,10 +185,10 @@ class GenericBinarySensor(BinarySensorBase):
 
 
 class HATydom(Entity):
-    """Representation of a Tydom."""
+    """Representation of a Tydom Gateway."""
 
-    _attr_has_entity_name = True
-    _attr_entity_category = EntityCategory.DIAGNOSTIC
+    _attr_has_entity_name = False
+    _attr_entity_category = None
     entity_description: str
 
     should_poll = False
@@ -220,15 +223,9 @@ class HATydom(Entity):
 
     def __init__(self, device: Tydom) -> None:
         self.device = device
-        self._attr_unique_id = f"{self.device.device_id}"
+        self._attr_unique_id = f"{self.device.device_id}_gateway"
         self._attr_name = self.device.device_name
         self._registered_sensors = []
-
-
-    @property
-    def is_on(self):
-        """Return the state of the sensor."""
-        return self._device.update_available
 
     async def async_added_to_hass(self) -> None:
         """Run when this Entity has been added to HA."""
@@ -304,7 +301,11 @@ class HATydom(Entity):
 
 
 class HAEnergy(Entity):
-    """Representation of an energy sensor"""
+    """Representation of an Energy sensor"""
+
+    _attr_has_entity_name = False
+    _attr_entity_category = None
+    entity_description: str
 
     should_poll = False
     device_class = None
@@ -335,16 +336,6 @@ class HAEnergy(Entity):
         "energyIndexHeatGas": SensorDeviceClass.ENERGY,
         "outTemperature": SensorDeviceClass.TEMPERATURE,
     }
-
-
-    #MEASUREMENT = "measurement"
-    """The state represents a measurement in present time."""
-    #TOTAL = "total"
-    """The state represents a total amount.
-    For example: net energy consumption"""
-    #TOTAL_INCREASING = "total_increasing"
-    """The state represents a monotonically increasing total.
-    For example: an amount of consumed gas"""
 
     state_classes = {
         "energyTotIndexWatt": SensorStateClass.TOTAL_INCREASING,
@@ -381,11 +372,20 @@ class HAEnergy(Entity):
         "outTemperature": UnitOfTemperature.CELSIUS,
     }
 
-    def __init__(self, energy: TydomEnergy) -> None:
-        self._energy = energy
-        self._attr_unique_id = f"{self._energy.device_id}_energy"
-        self._attr_name = self._energy.device_name
+    def __init__(self, device: TydomEnergy) -> None:
+        self._device = device
+        self._attr_unique_id = f"{self._device.device_id}_energy"
+        self._attr_name = self._device.device_name
         self._registered_sensors = []
+        
+    # This property is important to let HA know if this entity is online or not.
+    # If an entity is offline (return False), the UI will refelect this.
+    @property
+    def available(self) -> bool:
+        """Return True if roller and hub is available."""
+        # FIXME
+        # return self._device.online and self._device.hub.online
+        return True
 
     async def async_added_to_hass(self) -> None:
         """Run when this Entity has been added to HA."""
@@ -395,12 +395,12 @@ class HAEnergy(Entity):
         # called where ever there are changes.
         # The call back registration is done once this entity is registered with HA
         # (rather than in the __init__)
-        self._energy.register_callback(self.async_write_ha_state)
+        self._device.register_callback(self.async_write_ha_state)
 
     async def async_will_remove_from_hass(self) -> None:
         """Entity being removed from hass."""
         # The opposite of async_added_to_hass. Remove any registered call backs here.
-        self._energy.remove_callback(self.async_write_ha_state)
+        self._device.remove_callback(self.async_write_ha_state)
 
     # To link this entity to the cover device, this property must return an
     # identifiers value matching that used in the cover, but no other information such
@@ -410,15 +410,15 @@ class HAEnergy(Entity):
     def device_info(self):
         """Return information to link this entity with the correct device."""
         return {
-            "identifiers": {(DOMAIN, self._energy.device_id)},
-            "name": self._energy.device_name,
+            "identifiers": {(DOMAIN, self._device.device_id)},
+            "name": self._device.device_name,
         }
-
+    
     def get_sensors(self):
         """Get available sensors for this entity"""
         sensors = []
 
-        for attribute, value in self._energy.__dict__.items():
+        for attribute, value in self._device.__dict__.items():
             if (
                 attribute[:1] != "_"
                 and value is not None
@@ -439,12 +439,12 @@ class HAEnergy(Entity):
                 if isinstance(value, bool):
                     sensors.append(
                         GenericBinarySensor(
-                            self._energy, sensor_class, attribute, attribute
+                            self._device, sensor_class, attribute, attribute
                         )
                     )
                 else:
                     sensors.append(
-                        GenericSensor(self._energy, sensor_class, state_class, attribute, attribute, unit)
+                        GenericSensor(self._device, sensor_class, state_class, attribute, attribute, unit)
                     )
                 self._registered_sensors.append(attribute)
 
@@ -475,24 +475,24 @@ class HACover(CoverEntity):
         "intrusion": BinarySensorDeviceClass.PROBLEM,
     }
 
-    def __init__(self, shutter: TydomShutter) -> None:
+    def __init__(self, device: TydomShutter) -> None:
         """Initialize the sensor."""
         # Usual setup is done here. Callbacks are added in async_added_to_hass.
-        self._shutter = shutter
+        self._device = device
 
         # A unique_id for this entity with in this domain. This means for example if you
         # have a sensor on this cover, you must ensure the value returned is unique,
         # which is done here by appending "_cover". For more information, see:
         # https://developers.home-assistant.io/docs/entity_registry_index/#unique-id-requirements
         # Note: This is NOT used to generate the user visible Entity ID used in automations.
-        self._attr_unique_id = f"{self._shutter.device_id}_cover"
+        self._attr_unique_id = f"{self._device.device_id}_cover"
 
         # This is the name for this *entity*, the "name" attribute from "device_info"
         # is used as the device name for device screens in the UI. This name is used on
         # entity screens, and used to build the Entity ID that's used is automations etc.
-        self._attr_name = self._shutter.device_name
+        self._attr_name = self._device.device_name
         self._registered_sensors = []
-        if hasattr(shutter, "position"):
+        if hasattr(device, "position"):
             self.supported_features = (
                 self.supported_features
                 | SUPPORT_SET_POSITION
@@ -500,7 +500,7 @@ class HACover(CoverEntity):
                 | SUPPORT_CLOSE
                 | SUPPORT_STOP
             )
-        if hasattr(shutter, "slope"):
+        if hasattr(device, "slope"):
             self.supported_features = (
                 self.supported_features
                 | SUPPORT_SET_TILT_POSITION
@@ -517,12 +517,12 @@ class HACover(CoverEntity):
         # called where ever there are changes.
         # The call back registration is done once this entity is registered with HA
         # (rather than in the __init__)
-        self._shutter.register_callback(self.async_write_ha_state)
+        self._device.register_callback(self.async_write_ha_state)
 
     async def async_will_remove_from_hass(self) -> None:
         """Entity being removed from hass."""
         # The opposite of async_added_to_hass. Remove any registered call backs here.
-        self._shutter.remove_callback(self.async_write_ha_state)
+        self._device.remove_callback(self.async_write_ha_state)
 
     # Information about the devices that is partially visible in the UI.
     # The most critical thing here is to give this entity a name so it is displayed
@@ -547,12 +547,9 @@ class HACover(CoverEntity):
     def device_info(self) -> DeviceInfo:
         """Information about this entity/device."""
         return {
-            "identifiers": {(DOMAIN, self._shutter.device_id)},
+            "identifiers": {(DOMAIN, self._device.device_id)},
             # If desired, the name for the device could be different to the entity
             "name": self.name,
-            # "sw_version": self._shutter.firmware_version,
-            # "model": self._shutter.model,
-            # "manufacturer": self._shutter.hub.manufacturer,
         }
 
     # This property is important to let HA know if this entity is online or not.
@@ -560,7 +557,7 @@ class HACover(CoverEntity):
     @property
     def available(self) -> bool:
         """Return True if roller and hub is available."""
-        # return self._shutter.online and self._shutter.hub.online
+        # return self._device.online and self._device.hub.online
         # FIXME
         return True
 
@@ -575,70 +572,70 @@ class HACover(CoverEntity):
     @property
     def current_cover_position(self):
         """Return the current position of the cover."""
-        return self._shutter.position
+        return self._device.position
 
     @property
     def is_closed(self) -> bool:
         """Return if the cover is closed, same as position 0."""
-        return self._shutter.position == 0
+        return self._device.position == 0
 
     @property
     def current_cover_tilt_position(self):
         """Return the current tilt position of the cover."""
-        if hasattr(self._shutter, "slope"):
-            return self._shutter.slope
+        if hasattr(self._device, "slope"):
+            return self._device.slope
         else:
             return None
 
     # @property
     # def is_closing(self) -> bool:
     #    """Return if the cover is closing or not."""
-    #    return self._shutter.moving < 0
+    #    return self._device.moving < 0
 
     # @property
     # def is_opening(self) -> bool:
     #    """Return if the cover is opening or not."""
-    #    return self._shutter.moving > 0
+    #    return self._device.moving > 0
 
     # These methods allow HA to tell the actual device what to do. In this case, move
     # the cover to the desired position, or open and close it all the way.
     async def async_open_cover(self, **kwargs: Any) -> None:
         """Open the cover."""
-        await self._shutter.up()
+        await self._device.up()
 
     async def async_close_cover(self, **kwargs: Any) -> None:
         """Close the cover."""
-        await self._shutter.down()
+        await self._device.down()
 
     async def async_stop_cover(self, **kwargs):
         """Stop the cover."""
-        await self._shutter.stop()
+        await self._device.stop()
 
     async def async_set_cover_position(self, **kwargs: Any) -> None:
         """Set the cover's position."""
-        await self._shutter.set_position(kwargs[ATTR_POSITION])
+        await self._device.set_position(kwargs[ATTR_POSITION])
 
     async def async_open_cover_tilt(self, **kwargs):
         """Open the cover tilt."""
-        await self._shutter.slope_open()
+        await self._device.slope_open()
 
     async def async_close_cover_tilt(self, **kwargs):
         """Close the cover tilt."""
-        await self._shutter.slope_close()
+        await self._device.slope_close()
 
     async def async_set_cover_tilt_position(self, **kwargs):
         """Move the cover tilt to a specific position."""
-        await self._shutter.set_slope_position(kwargs[ATTR_TILT_POSITION])
+        await self._device.set_slope_position(kwargs[ATTR_TILT_POSITION])
 
     async def async_stop_cover_tilt(self, **kwargs):
         """Stop the cover tilt."""
-        await self._shutter.slope_stop()
+        await self._device.slope_stop()
 
     def get_sensors(self) -> list:
         """Get available sensors for this entity"""
         sensors = []
 
-        for attribute, value in self._shutter.__dict__.items():
+        for attribute, value in self._device.__dict__.items():
             if (
                 attribute[:1] != "_"
                 and value is not None
@@ -650,12 +647,12 @@ class HACover(CoverEntity):
                 if isinstance(value, bool):
                     sensors.append(
                         GenericBinarySensor(
-                            self._shutter, sensor_class, attribute, attribute
+                            self._device, sensor_class, attribute, attribute
                         )
                     )
                 else:
                     sensors.append(
-                        GenericSensor(self._shutter, sensor_class, None, attribute, attribute, None)
+                        GenericSensor(self._device, sensor_class, None, attribute, attribute, None)
                     )
                 self._registered_sensors.append(attribute)
 
@@ -663,20 +660,21 @@ class HACover(CoverEntity):
 
 
 class HASmoke(BinarySensorEntity):
-    """Representation of an smoke sensor"""
+    """Representation of an Smoke sensor"""
 
     should_poll = False
-    device_class = BinarySensorDeviceClass.PROBLEM
     supported_features = None
 
     sensor_classes = {"batt_defect": BinarySensorDeviceClass.PROBLEM}
 
-    def __init__(self, smoke: TydomSmoke) -> None:
-        self._device = smoke
+    def __init__(self, device: TydomSmoke) -> None:
+        self._device = device
         self._attr_unique_id = f"{self._device.device_id}_smoke_defect"
         self._attr_name = self._device.device_name
         self._state = False
         self._registered_sensors = []
+        self._attr_device_class = BinarySensorDeviceClass.SMOKE
+
 
     @property
     def is_on(self):
@@ -775,54 +773,17 @@ class HaClimate(ClimateEntity):
         self._attr_hvac_modes = [
             HVACMode.OFF,
             HVACMode.HEAT,
-        ]  # , HVACMode.AUTO, HVACMode.COOL,
+        ]
         self._registered_sensors = []
 
         self._attr_preset_modes = ["NORMAL", "STOP", "ANTI_FROST"]
-        #self._attr_preset_mode = "STOP"
         self._attr_hvac_modes = [HVACMode.OFF, HVACMode.HEAT]
-        # self._attr_hvac_mode = DICT_MODES_DD__TO_HA[]
-    """
-    {
-                  "name":"authorization",
-                  "type":"string",
-                  "permission":"rw",
-                  "validity":"STATUS_POLLING",
-                  "enum_values":[
-                     "STOP",
-                     "HEATING"
-                  ]
-
-
-                                 {
-                  "name":"hvacMode",
-                  "type":"string",
-                  "permission":"rw",
-                  "validity":"DATA_POLLING",
-                  "enum_values":[
-                     "NORMAL",
-                     "STOP",
-                     "ANTI_FROST"
-                  ]
-    
-    """
-
+ 
     @property
     def supported_features(self) -> ClimateEntityFeature:
         """Return the list of supported features."""
         features = ClimateEntityFeature(0)
-
         features = features | ClimateEntityFeature.TARGET_TEMPERATURE | ClimateEntityFeature.PRESET_MODE
-        # set_req = self.gateway.const.SetReq
-        # if set_req.V_HVAC_SPEED in self._values:
-        #    features = features | ClimateEntityFeature.FAN_MODE
-        # if (
-        #    set_req.V_HVAC_SETPOINT_COOL in self._values
-        #    and set_req.V_HVAC_SETPOINT_HEAT in self._values
-        # ):
-        #    features = features | ClimateEntityFeature.TARGET_TEMPERATURE_RANGE
-        # else:
-        #    features = features | ClimateEntityFeature.TARGET_TEMPERATURE
         return features
 
     @property
@@ -933,7 +894,7 @@ class HaClimate(ClimateEntity):
 
 
 class HaWindow(CoverEntity):
-    """Representation of a Cover."""
+    """Representation of a Window."""
 
     should_poll = False
     supported_features = None
@@ -1002,7 +963,7 @@ class HaWindow(CoverEntity):
 
 
 class HaDoor(LockEntity, CoverEntity):
-    """Representation of a Cover."""
+    """Representation of a Door."""
 
     should_poll = False
     supported_features = None
@@ -1071,7 +1032,7 @@ class HaDoor(LockEntity, CoverEntity):
 
 
 class HaGate(CoverEntity):
-    """Representation of a Cover."""
+    """Representation of a Gate."""
 
     should_poll = False
     supported_features = None
@@ -1131,7 +1092,7 @@ class HaGate(CoverEntity):
 
 
 class HaGarage(CoverEntity):
-    """Representation of a Cover."""
+    """Representation of a Garage door."""
 
     should_poll = False
     supported_features = None
