d4705872d3756c41b8c868f045eed28917f537a3
CyrilP <5814027+CyrilP@users.noreply.github.com>
2023-11-13 11:28:09 +0100

    update domain name (Domain can consist out of numbers 0 to 9, lower case letters a to z, and the underscore _)

--- custom_components/deltadore-tydom/const.py
+++ custom_components/deltadore-tydom/const.py
@@ -5,7 +5,7 @@ LOGGER: Logger = getLogger(__package__)
 
 # This is the internal name of the integration, it should also match the directory
 # name for the integration.
-DOMAIN = "deltadore-tydom"
+DOMAIN = "deltadore_tydom"
 NAME = "Integration blueprint"
 VERSION = "0.0.1"
 ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
