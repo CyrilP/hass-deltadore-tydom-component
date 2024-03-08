8b2fd40c23598ebd49613cc37f435704cce5894d
cyrilp <cyril@kali.home>
2024-03-08 13:52:31 +0100

    add script to cleanup logs for use with file mode

---
+++ cleanlogs.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#cat $1 | grep "Incomming message" | sed "s/.*b'//g" 
+cat $1 | grep "Incomming message" | sed "s/.*b'//g" | sed "s/'$//g" |sed "s/\\\\x[a-f0-9][a-f0-9]/X/g" | sed "s/\\\\'/'/g"
