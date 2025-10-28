e6f25263ec94cfd3ecf479f594da91416648572f
CyrilP <5814027+CyrilP@users.noreply.github.com>
2025-10-28 19:06:47 +0100

    remove unused imports

--- custom_components/deltadore_tydom/binary_sensor.py
+++ custom_components/deltadore_tydom/binary_sensor.py
@@ -1,10 +1,6 @@
 """Platform for sensor integration."""
 
-import logging
-from homeassistant.components.binary_sensor import BinarySensorEntity
 from .const import DOMAIN
-
-_LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass, entry, async_add_entities):
     """Set up binary sensors for Deltadore windows."""
